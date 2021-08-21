74eb18eaf332bd773ea4033d71688f032e561b5f
Your Name <nguenviktoria@gmail.com>
2021-08-21 13:40:04 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  array.sort(function (a, b) {
+    return a - b;
+  })
+  return array[0];
+  
+};
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
   return 0;
-}
+  }
+  array.sort(function (a, b) {
+    return b - a;
+  })
+  return array[0];
+  
+};
 
 exports.avg = function avg (array) {
+  if (array === undefined || array.length === 0) {
   return 0;
-}
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return (sum / array.length);
+  
+};
